992d769490ba59e5aed9a8a9203ab3892f11c7e6
katherinetian540 <rui.tian@duke.edu>
2023-09-14 22:20:28 -0400

    git commit -m '1st commit'

--- main.py
+++ main.py
@@ -2,18 +2,26 @@
 Main function goes here
 """
 
-#import pandas and pyplot
+#import polars and pyplot
+import polars as pl
 import matplotlib.pyplot as plt
 
+#read dataset
+cereal = pl.read_csv("cereal.csv",separator = ';')
+
 #define summary statistics function
-def sum_stats(data):
+def polar_stats(data):
     return data.describe()
  
 #define visualization function
-def visualization(data):
+def polar_visualization(data):
     plt.hist(data["calories"], bins=10, color="purple")
     plt.xlabel("Calories")
     plt.ylabel("Frequency")
     plt.title("Cereal Calories Histogram")
     plt.show()
     return
+
+if __name__ == "__main__":
+    print(polar_stats(cereal))
+    polar_visualization(cereal)
--- test_main.py
+++ test_main.py
@@ -2,20 +2,19 @@
 Test Cases
 """
 #import functions
-from main import sum_stats, visualization
-import pandas as pd
+from main import polar_stats, polar_visualization
+import polars as pl
 
 #define test function
 def test_sum_stats():
     #read dataset
-    cereal = pd.read_csv("cereal.csv",sep=";")
-    summary = sum_stats(cereal)
-    assert cereal['calories'].mean() == summary.loc['mean', 'calories'], "Mean test failed"
-    assert cereal['protein'].median() == summary.loc['50%', 'protein'], "Median test failed"
-    assert cereal['rating'].min() == summary.loc['min', 'rating'], "Standard deviation test failed"
+    cereal = pl.read_csv("cereal.csv",separator = ';')
+    summary = polar_stats(cereal)
+    assert cereal['calories'].mean() == summary["calories"][2], "Mean test failed"
+    assert cereal['protein'].median() == summary['protein'][6], "Median test failed"
+    assert cereal['rating'].min() == summary['rating'][4], "Standard deviation test failed"
     print("All Test passed!")
 
 #test
 if __name__ == "__main__":
     test_sum_stats()
-    
